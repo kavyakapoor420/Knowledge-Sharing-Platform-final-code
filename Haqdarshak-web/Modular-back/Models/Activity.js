edf7e0281042efee49188a1dcc29cea401fb2d02
Kavyakapoor <167117026+kavyakapoor420@users.noreply.github.com>
2026-01-05 18:06:29 +0530

    Delete Haqdarshak-web directory

--- Haqdarshak-web/Modular-back/Models/Activity.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const activitySchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  date: { type: String, required: true }, // YYYY-MM-DD
-  count: { type: Number, default: 0 }
-});
-
-module.exports = mongoose.model('Activity', activitySchema);
